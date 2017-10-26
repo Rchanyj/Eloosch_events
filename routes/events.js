b25f433d3c464e17152b9b192f41ab227ce6f96b
bgk <blakegraysonkehler@gmail.com>
2017-10-26 13:27:19 -0700

    Add new migration

---
+++ routes/events.js
@@ -0,0 +1,99 @@
+"use strict";
+
+const express = require("express");
+const router = express.Router();
+const {randomKey} = require("../db/math");
+// Move knex to DataHelper.js
+
+module.exports = (DataHelpers) => {
+  router.post("/", (req, res) => {
+    const newEvent = { 
+      id: randomKey(),
+      name : "testing event!",
+      creator : "Tester McTest",
+      days: {
+        "2017-11-01": {
+          startTime: 1700,
+          endTime: 2100
+        },
+        "2017-11-02": {
+          startTime: undefined,
+          endTime: undefined
+        }
+      },
+      createdAt: new Date()
+    };
+    // Post to database with event info
+    DataHelpers.createEvent(newEvent)
+      // .then( event => console.log(event));
+      // .then( event => res.redirect("/events/" + event.id))
+  });
+  router.get("/:event_id", (req, res) => {
+    // Retreive event info from db
+    DataHelpers.getEvent(req.params.event_id)
+      // .then( event => res.render("events/:id", event))
+    // Callback res.render("/events/:id")
+  });
+  router.put("/:event_id", (req, res) => {
+    // Edit event info
+    // DataHelpers.editEvent("event_id") RETURN {event}
+    // Callback res.render("/events/event_:id")
+  });
+  router.delete("/:event_id", (req, res) => {
+    // Delete event in database
+    // DataHelpers.deleteEvent("id")
+    // Callback res.render("/")
+  });
+
+  router.post("/:event_id/votes", (req, res) => {
+    // Post votes to database
+    // DataHelpers.submitVotes("event_id", {votes}) RETURN {event}
+    // Callback res.render("/events/:id")
+  });
+  router.put("/:event_id/votes", (req, res) => {
+    // Edit votes in database
+    // DataHelpers.editVotes("event_id, {votes}") RETURN {event}
+    // Callback res.render("/events/:id", {event})
+  });
+
+  return router;
+};
+
+// {votes} = {
+//   attendee: "someName",
+//   days: {
+//     Date: true,
+//     Date: false,
+//     Date: true
+//   }
+//   hash: localStorage.hash,
+//   email: "address",
+// }
+
+
+// {event} = {
+//   creatorId,
+//   event_name,
+//   days: [
+//     2017-10-30: {
+//       times: {
+//         start: 1300
+//         end: 1900
+//       }
+//       attendees: {
+//         person1 : true,
+//         person2 : false,
+//       }
+//     },
+//     2017-10-31: {
+//       times: {
+//         start: 1900
+//         end: 2300
+//       },
+//       attendees: {
+//         person1: false,
+//         person2: true,
+//       }
+//     }
+//   ],  
+// }
