a44c99e69ec226fb5e30471d7e1ddad0e909c400
lankypete <lankypete7@gmail.com>
2017-10-30 04:32:37 +0000

    Committing to seed

--- routes/events.js
+++ routes/events.js
@@ -25,7 +25,7 @@ module.exports = (DataHelpers) => {
       days
     }
     // Post to database with event info
-
+    console.log(newEvent)
     DataHelpers.createEvent(newEvent)
       .then((ids) => {
         res.status(200).json(ids)
